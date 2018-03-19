4cea5351936b1577eff04224f699c2d17ebc4a08
Rina Mito <satoshikibu@gmail.com>
2018-03-19 23:00:13 +0900

    その11修正

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Idea, type: :model do
     idea = Idea.create!(name: "My Awesome Idea Name") # creating a new idea 'instance'
     second_idea = Idea.create!(name: "My Second Idea Name") # creating another new idea 'instance'
     expect(second_idea.name).to eq("My Second Idea Name") # this is our expectation
-    it "has a description"
+
   end
+  it "has a description"
 end
